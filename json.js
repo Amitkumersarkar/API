2bb0fb06aeef83c82251e7b36fbbe7b8d0641cd8
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-01 14:57:40 +0600

    added json code

---
+++ json.js
@@ -0,0 +1,9 @@
+const user = {
+    id: 1,
+    name: "Amit",
+    job: 'Developer'
+};
+// javascript object notation (json)
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
