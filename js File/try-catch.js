77b595a7404c93bc673929b5ddd385e74fe6fc54
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-03 00:58:31 +0600

    added error handling

--- js File/try-catch.js
+++ js File/try-catch.js
@@ -1,14 +1,23 @@
 function checkAge() {
     const ageField = document.getElementById('age');
     const ageText = ageField.ariaValueMax;
-
+    const errorTag = - document.getElementById('error');
 
     try {
-        console.log(mirzapur);
+        // console.log(Dhaka);
+        const age = parseInt(ageText);
 
+        if (isNaN(age)) {
+            throw "please enter a number";
+        }
     }
     catch (err) {
-        console.log('ERROR : ', err)
+        console.log('ERROR : ', err);
+        // errorTag.innerHtml = 'something wrong';
+        errorTag.innerHTML = 'ERROR : ' + err;
+    }
+    finally {
+        console.log('All okay inside try-catch');
     }
     console.log(111111);
 }
