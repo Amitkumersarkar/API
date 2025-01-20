b826bae0397885b49e43a2026bdef2500755dcc1
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-20 18:52:47 +0600

    added new code

---
+++ js File/cart.js
@@ -0,0 +1,10 @@
+const addProduct = () => {
+    const productField = document.getElementById('product-name');
+    const quantityField = document.getElementById('product-quantity');
+    const product = productField.value;
+    const quantity = quantityField.value;
+    productField.value = '';
+    quantityField.value = '';
+    console.log(product, quantity);
+
+}
